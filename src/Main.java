79a01357c2005e1c5f9b990ac82368346cae1c2b
wiggelyjiggely <wiggelyjiggely@hotmail.com>
2017-05-14 13:43:52 +0200

    beeteke gedaan

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
-	private final Visual Visual;
+	private final Visual visual;
 	private int algoritme;
 	private Algorithm algo;
 	private ArrayList<Cirkel> cirkels = new ArrayList<Cirkel>();
@@ -13,7 +19,8 @@ public class Main {
 	}
 	
 	public Main(){
-		Visual = new Visual(this);
+		this.visual = new Visual(this);
+		visual.setVisible(true);
 	}
 	
 	public void setAlg(int algorithm1){
@@ -61,6 +68,35 @@ public class Main {
 	
 	public ArrayList<Punt> getSnijpunten(){
 		return this.snijpunten;
+	}
+	
+	/*
+	 * reads Input.txt file and casts it to circles
+	 */
+	public ArrayList<Cirkel> readInput(File file){
+		ArrayList<Cirkel> cirkels = new ArrayList<Cirkel>();
+		
+		Scanner scan;
+		try {
+			scan = new Scanner(file);
+			setAlg(Integer.parseInt(scan.nextLine()));
+			int amountOfCirkels = Integer.parseInt(scan.nextLine());
+			for(int i = 0; i < amountOfCirkels; i++){
+				Cirkel cirkel = castToCirkel(scan.nextLine());
+				cirkels.add(cirkel);
+				return(cirkels);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}return cirkels;
+	}
+	
+	public Cirkel castToCirkel(String cirkel){
+		String[] parts = cirkel.split(" ");
+		Punt middelpunt = new Punt(Double.parseDouble(parts[0]), Double.parseDouble(parts[1]));
+		Cirkel castedCirkel = new Cirkel(middelpunt, Double.parseDouble(parts[2]));
+		return castedCirkel;
 	}
 	
 }
